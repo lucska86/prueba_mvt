069d6bf427b4a005233f5f1c6b2398f87fc033f4
Lucas Daniel Jofre <lucska866@gmail.com>
2022-10-02 13:20:42 -0300

    Prueba MVT fin

--- pruebaMvt/settings.py
+++ pruebaMvt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from re import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,8 @@ ROOT_URLCONF = 'pruebaMvt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [r'C:\Users\Usuario\Desktop\principal\prueba_mvt\pruebaMvt\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
